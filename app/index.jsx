be0c082565f214d1f52476a0b35116f4a790480b
yjw9012 <yjw9012@hotmail.com>
2016-01-07 00:07:52 -0800

    Add pinterest sdk and initialize PDK TODO: error is thrown on PDK.login()

--- app/index.jsx
+++ app/index.jsx
@@ -5,6 +5,15 @@ import ReactDOM from 'react-dom';
 
 var ImageSearchApp = require('./components/ImageSearchApp.jsx');
 
+require('./pinterest/sdk.js');
+PDK.init({
+    appId: "4810864636186345530", // Change this
+    cookie: true
+});
+
+// why error here?
+PDK.login({ scope : 'read_public' }, ()=>{});
+
 main();
 
 function main() {
